0db710f2fbad5de23f0faf71b425953ab849594a
Drack Sebastian <sebastian.drack@ict.de>
2020-04-09 15:38:04 +0200

    fill the basic content

---
+++ APNOA_V4_MAIN.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+start C:\vvvv\vvvv_beta_39_x64\vvvv.exe /server /allowmultiple /showexceptions false /r D:\APNOA_V4\CONTENT\root.v4p /o D:\APNOA_V4\CONTENT\MAIN.v4p 
---
+++ _vvvv.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+start vvvv_alpha_x64\vvvv.exe --Redis 127.0.0.1:8889 /allowmultiple /showexceptions false /r ..\_patches\root.v4p
