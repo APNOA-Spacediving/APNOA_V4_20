b3b9583dcb7e8d5f1897725dcd7531a48bf3198b
unknown <tobias@m-box.de>
2020-04-13 23:27:42 +0200

    splines reactive on x

--- APNOA_V4_MAIN.bat
+++ APNOA_V4_MAIN.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-start C:\vvvv\vvvv_beta_39_x64\vvvv.exe /server /allowmultiple /showexceptions false /r D:\APNOA_V4\CONTENT\root.v4p /o D:\APNOA_V4\CONTENT\MAIN.v4p 
+start C:\vvvv\vvvv_beta_39_x64\vvvv.exe /server /allowmultiple /showexceptions false /r D:\APNOA_V4_20\CONTENT\root.v4p /o D:\APNOA_V4_20\CONTENT\MAIN.v4p 
